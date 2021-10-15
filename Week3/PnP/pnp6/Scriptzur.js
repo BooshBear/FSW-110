849a1fba463dcdeeebe8a67884a212c9dcdb67ca
BooshBear <dylan.mailloux@bryanuniversity.edu>
2021-10-14 17:55:49 -0700

    Part6

---
+++ Week3/PnP/pnp6/Scriptzur.js
@@ -0,0 +1,15 @@
+for (let i = 0; i < 5; i++) {
+    var head2 = document.createElement("h2");
+    head2.textContent = "Hello Warldo";
+    head2.style.fontSize = "20px";
+    head2.style.fontWeight = "lighter";
+    head2.style.fontFamily = "sans-serif";
+    head2.style.color = "cornflowerblue";
+    head2.classList += "border";
+    document.body.appendChild(head2);
+}
+
+
+
+
+
