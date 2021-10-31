fc86f0f45bfaa1037f0be22d3ae9b6906ccbdef1
BooshBear <dylan.mailloux@bryanuniversity.edu>
2021-10-30 21:52:07 -0700

    part8

---
+++ Week5/PnP/pnp8/daScript.js
@@ -0,0 +1,7 @@
+let form = document.forms["myForm"]["fin"].value;
+let button = document.getElementById("btn");
+button.addEventListener("click", function(){
+    alert(document.getElementById("1").value);
+    alert(document.getElementById("2").value)
+    alert(document.getElementById("3").value)
+})
