4d19aa2899a5c1a7068ce3fa4397929c3bc1b0ba
BooshBear <dylan.mailloux@bryanuniversity.edu>
2021-11-07 18:46:00 -0800

    Projecto

---
+++ Week6/Project/daScript.js
@@ -0,0 +1,16 @@
+let sum = document.getElementById("result");
+
+sum.addEventListener("click", function(){
+    let bagel = document.getElementById("bagel").value;
+    let bagelCost = bagel*3;
+    let tea = document.getElementById("tea").value;
+    let teaCost = tea*7;
+    let coffee = document.getElementById("coffee").value;
+    let coffeeCost = coffee*11;
+    let total = bagelCost + teaCost + coffeeCost;
+
+    let h2 = document.createElement("h2");
+    let text = document.createTextNode("The cost will be $" + total + ".00 please.");
+    h2.appendChild(text);
+    document.body.appendChild(h2);
+})
