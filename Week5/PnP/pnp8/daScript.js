c9be71c9be9513be6eda92c797b34d76c8f4c49d
BooshBear <dylan.mailloux@bryanuniversity.edu>
2021-11-13 07:28:01 -0800

    Resubmit

--- Week5/PnP/pnp8/daScript.js
+++ Week5/PnP/pnp8/daScript.js
@@ -1,7 +1,8 @@
 let form = document.forms["myForm"]["fin"].value;
 let button = document.getElementById("btn");
 button.addEventListener("click", function(){
-    alert(document.getElementById("1").value);
-    alert(document.getElementById("2").value)
-    alert(document.getElementById("3").value)
+    var a = document.getElementById("1").value;
+    var b = document.getElementById("2").value;
+    var c = document.getElementById("3").value;
+    alert(a +" "+b+" "+ c);
 })
--- Week5/PnP/pnp9/daScript.js
+++ Week5/PnP/pnp9/daScript.js
@@ -5,12 +5,12 @@ button.addEventListener("click", function(){
     var c = document.forms["myForm"]["age"].value;
     var d = document.forms["myForm"]["yes_no"].value;
     var e = document.getElementById("recommend").value;
-    var f = document.forms["myForm"]["dietR"].value;
+    var f = document.querySelector(".dietR:checked").value;
 
     alert("First Name: " +a+
     "\nSecond Name: " +b+ 
     "\nAge: " +c+
-    "\nHave you shopped with us before? " +d+
-    "\nHow likely would you recommend this to a friend? " +e+ 
-    "\nDo you have any dietary restrictions? " + f)
+    "\nHave you shopped with us before? = " +d+
+    "\nHow likely would you recommend this to a friend? = " +e+ 
+    "\nDo you have any dietary restrictions? = " + f)
 })
