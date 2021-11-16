a76ad011791ff37977bcdec8938e9ef267a329c8
BooshBear <dylan.mailloux@bryanuniversity.edu>
2021-11-16 12:40:43 -0800

    Finsihed

--- Week7/Capstone/script.js
+++ Week7/Capstone/script.js
@@ -3,35 +3,26 @@ var submit = document.getElementById("submit");
 submit.addEventListener("click", function(e){
     e.preventDefault();
 
-    var name = document.getElementById("name").value;
-    var color = document.getElementById("color").value;
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
     
     var deleteMe = document.createElement("button");
     deleteMe.innerHTML = "Delete ->";
 
     var li = document.createElement("li");
-    var text = document.createTextNode(" Name: " + name);
+    var text = document.createTextNode(" Name: " + fname +" "+ lname);
     li.appendChild(deleteMe);
     li.appendChild(text);
     
     var deleteMe2 = document.createElement("button");
     deleteMe2.innerHTML = "Delete ->";
 
-    var li2 = document.createElement("li");
-    var text2 = document.createTextNode(" Color in code: " + color);
-    li2.appendChild(deleteMe2);
-    li2.appendChild(text2);
-
     document.querySelector("ul").appendChild(li);
-    document.querySelector("ul").appendChild(li2);
 
-    document.getElementById("name").value = "";
-    document.getElementById("color").value = "";
+    document.getElementById("fname").value = "";
+    document.getElementById("lname").value = "";
 
 deleteMe.addEventListener("click", function(){
-    this.closest("li").remove();
-})
-deleteMe2.addEventListener("click", function(){
     this.closest("li").remove();
 })
 
